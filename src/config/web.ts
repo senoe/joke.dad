cace7f6b650df6930b8c5fc45e20dd16020522cb
senoe <47705140+senoe@users.noreply.github.com>
2023-05-27 00:20:23 -0700

    fix: metadata

--- src/config/web.ts
+++ src/config/web.ts
@@ -1,10 +1,9 @@
 import { type Metadata } from 'next';
-import { slate } from 'tailwindcss/colors';
+import { sky } from 'tailwindcss/colors';
 
 export const webConfig = {
   name: 'joke.dad',
   url: 'https://joke.dad',
-  ogImage: '/og.jpg',
   description: 'Where the puns are dadly and the laughs are grand.',
   links: {
     github: 'https://github.com/senoe/joke.dad'
@@ -15,8 +14,7 @@ export const webMetadata: Metadata = {
   metadataBase: new URL(webConfig.url),
   applicationName: webConfig.name,
   description: webConfig.description,
-  manifest: '/site.webmanifest',
-  themeColor: slate['900'],
+  themeColor: sky['500'],
   title: {
     default: webConfig.name,
     template: `%s | ${webConfig.name}`
@@ -32,26 +30,14 @@ export const webMetadata: Metadata = {
     url: webConfig.url,
     title: webConfig.name,
     siteName: webConfig.name,
-    images: [webConfig.ogImage],
     description: webConfig.description
   },
   twitter: {
-    card: 'summary_large_image',
     title: webConfig.name,
-    images: [webConfig.ogImage],
     description: webConfig.description
   },
   appleWebApp: {
     title: webConfig.name
-  },
-  icons: {
-    icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
-    ],
-    shortcut: '/favicon-16x16.png',
-    apple: { url: '/apple-touch-icon.png', sizes: '180x180' }
   },
   keywords: [
     'dad joke',
