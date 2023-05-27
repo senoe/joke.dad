5e1d17bcd183a1152a32b541d0d3758278739a68
senoe <47705140+senoe@users.noreply.github.com>
2023-05-27 00:02:51 -0700

    fix: route naming

--- src/app/api/list/_route.ts
+++ src/app/api/list/_route.ts
@@ -2,7 +2,7 @@ import { prisma } from '@/server/db';
 import { NextResponse } from 'next/server';
 
 // For testing purposes
-export async function _GET() {
+export async function GET() {
   const jokes = await prisma.joke.findMany({
     select: { name: true, content: true }
   });
